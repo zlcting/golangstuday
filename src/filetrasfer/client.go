7df0adf5a9641307dd17ab3a801a51e01eda94c8
zlcting <zlcting@gmail.com>
2017-08-08 18:07:20 +0800

    123123

--- src/filetrasfer/client.go
+++ src/filetrasfer/client.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
+	"os"
 	"strconv"
 )
 
@@ -13,4 +15,30 @@ func runclient(port int, file string) {
 	}
 	defer conn.Close()
 	log.Println("请求成功")
+
+	f, err := os.Open(file)
+	if err != nil {
+		log.Printf("无法打开文件%s", file)
+		return
+	}
+	defer f.Close()
+	//写入头信息并等待确认
+	conn.Write([]byte(file))
+	p := make([]byte, 2)
+	_, err = conn.Read(p)
+	if err != nil {
+		log.Printf("无法jieshou%v", err)
+		return
+	} else if string(p) != "ok" {
+		log.Printf("无效的服务器响应%v", string(p))
+		return
+	}
+	log.Println("头信息发送成功")
+
+	_, err = io.Copy(conn, f)
+	if err != nil {
+		log.Printf("发送文件失败(%s):%v", file, err)
+		return
+	}
+	log.Println("wenjian发送成功")
 }
--- src/filetrasfer/server.go
+++ src/filetrasfer/server.go
@@ -1,10 +1,55 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
+	"os"
 	"strconv"
 )
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	remoteAdrr := conn.RemoteAddr().String()
+	log.Println("ip address:", remoteAdrr)
+	p := make([]byte, 1024)
+	n, err := conn.Read(p)
+	if err != nil {
+		log.Printf("读取文件头失败(%s)%v", remoteAdrr, err)
+		return
+	} else if n == 0 {
+		log.Printf("空文件头(%s)", remoteAdrr)
+		return
+	}
+	fileName := string(p[:n])
+	log.Printf("文件(%s)", fileName)
+	conn.Write([]byte("ok"))
+
+	//打开一个本地文件
+	os.MkdirAll("receive", os.ModePerm)
+	f, err := os.Create("receive/" + fileName)
+	if err != nil {
+		log.Printf("无法创建文件(%s):%v", remoteAdrr, fileName)
+		return
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, conn)
+	for {
+		buffer := make([]byte, 1024*200) //每次读取200字节可自定义
+		_, err := conn.Read(buffer)
+		if err != nil && err != io.EOF {
+			log.Printf("读取失败(%s):%v", remoteAdrr, err)
+			return
+		} else if err == io.EOF {
+			break
+		}
+	}
+	if err != nil {
+		log.Printf("文件接收失败(%s):%v", remoteAdrr, err)
+	}
+	log.Printf("文件接收成功(%s):%s", remoteAdrr, fileName)
+}
 
 func runserver(port int) {
 	l, err := net.Listen("tcp", ":"+strconv.Itoa(port))
@@ -22,6 +67,6 @@ func runserver(port int) {
 
 		}
 		log.Println("请求接收成功")
-		_ = conn
+		handle(conn)
 	}
 }
